7072b2d849def719af940493b245f4434ec0afc3
Vetradu10P <vetradu10pgmail.com>
2020-04-09 17:56:53 +0200

    api pour pharmacie 1

--- routes/pharmacie.js
+++ routes/pharmacie.js
@@ -6,6 +6,15 @@ const Pharmacie = require('../models/Pharmacie');
 const mongoose = require('mongoose');
 mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,{useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 
+router.get('/:pharmacie', function(req, res) {
+    console.log(req.params)
+   const pharmacie = req.params.pharmacie;
+    Pharmacie.find({nom:pharmacie}, (err, pharmacies) => {
+        if (err) throw err;
+        res.send(pharmacies);
+    })
+});
+
 /* liste pharmacie  */
 router.get('/', function(req, res) {
     Pharmacie.find({}, (err, pharmacies) => {
